0c41f9702bbaa718271d4f28253e59300532a980
technicallyawriter <npjones@protonmail.com>
2024-02-14 11:28:04 -0500

    make docs-as-code extremely modular

---
+++ modules/ROOT/examples/git/feature-branch-naming-convention-example.bash
@@ -0,0 +1,12 @@
+.Example of preferred feature branch naming conventions
+[source,bash]
+----
+$ git branch
+
+* main
+feature/jira-123
+feature/jira-456
+feature/jira-789
+release/v1.1.0
+hotfix/v1.0.4
+----
